b7a3a580256b576929570a1f21b2f6760ff8fe35
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2018-11-26 22:53:29 +0100

    Add implementation projects: individual and group request

---
+++ Implementation/grouprequestservice/src/test/java/com/poianitibaldizhou/trackme/grouprequestservice/GroupRequestServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.poianitibaldizhou.trackme.grouprequestservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupRequestServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ Implementation/individualrequestservice/src/main/java/com/poianitibaldizhou/trackme/individualrequestservice/IndividualRequestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.poianitibaldizhou.trackme.individualrequestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualRequestServiceApplication.class, args);
+	}
+}
