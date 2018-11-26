21de496d2356cf73b8f24330aaf052609a36411b
Tang-Tang Zhou <tangtang.zhou@mail.polimi.it>
2018-11-26 22:54:24 +0100

    Add new projects of message queue and share data service

---
+++ Implementation/sharedataservice/src/main/java/com/poianitibaldizhou/trackme/sharedataservice/ShareDataServiceApplication.java
@@ -0,0 +1,12 @@
+package com.poianitibaldizhou.trackme.sharedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareDataServiceApplication.class, args);
+	}
+}
