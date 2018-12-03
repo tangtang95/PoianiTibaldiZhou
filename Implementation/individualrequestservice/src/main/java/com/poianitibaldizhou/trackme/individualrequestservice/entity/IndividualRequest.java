8d8130f59f12e4b4fd37d766a444badd64426aab
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2018-12-03 22:55:33 +0100

    Add integration test for individual request manager service

--- Implementation/individualrequestservice/src/main/java/com/poianitibaldizhou/trackme/individualrequestservice/entity/IndividualRequest.java
+++ Implementation/individualrequestservice/src/main/java/com/poianitibaldizhou/trackme/individualrequestservice/entity/IndividualRequest.java
@@ -15,7 +15,7 @@ import java.sql.Timestamp;
 public class IndividualRequest {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
