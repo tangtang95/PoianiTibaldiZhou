4a7f71bf3544737bf2083b4896480fb7a00b1b13
Tiba96 <mattia.tibaldi@mail.polimi.it>
2018-12-17 13:02:13 +0100

    add accountNetwork

--- Implementation/trackMeApplication/app/src/main/java/com/trackme/trackmeapplication/account/login/UserLoginActivity.java
+++ Implementation/trackMeApplication/app/src/main/java/com/trackme/trackmeapplication/account/login/UserLoginActivity.java
@@ -8,57 +8,98 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.trackme.trackmeapplication.baseUtility.BaseAlertDialog;
+import com.trackme.trackmeapplication.baseUtility.Constant;
 import com.trackme.trackmeapplication.home.businessHome.BusinessHomeActivity;
 import com.trackme.trackmeapplication.home.userHome.UserHomeActivity;
 import com.trackme.trackmeapplication.R;
 
-import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
+/**
+ * UserLoginActivity extends the abstract class LoginActivity and it allows to the user to
+ * login into the application. It is the first Activity that starts when the user install
+ * the apk on his mobile device.
+ *
+ * @author Mattia Tibaldi
+ * @see LoginActivity
+ **/
 public class UserLoginActivity extends LoginActivity {
 
     @BindView(R.id.editTextUser)
     protected EditText username;
 
+    /**
+     * True if the broadcast receiver is registered, false otherwise.
+     */
+    private boolean isRegister;
+
+    /**
+     * it manages the interaction with other activity like businessLoginActivity. it is used mainly
+     * for finishing an activity from an other.
+     */
     BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
 
+        /**
+         * It stops the activity when the action sent is "finish_activity"
+         *
+         * @param arg0 the sender intent.
+         * @param intent action to perform.
+         */
         @Override
         public void onReceive(Context arg0, Intent intent) {
             String action = intent.getAction();
-            if (Objects.requireNonNull(action).equals("finish_activity")) {
-                finish();
+            if (action != null && action.equals(Constant.FINISH_ACTION)) {
                 unregisterReceiver(this);
+                isRegister = false;
+                finish();
+            } else {
+                BaseAlertDialog alertDialog = new BaseAlertDialog(
+                        arg0,
+                        getString(R.string.broadcast_receiver_error),
+                        getString(R.string.fatal_error_title));
+                alertDialog.show();
             }
         }
     };
 
+    /**
+     * Method call when this activity is created. It checks the past user session for verifying
+     * if the user has already logged and it chooses the best activity to start.
+     *
+     * @param savedInstanceState last saved instant state.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
 
-        sp = getSharedPreferences("login",MODE_PRIVATE);
+        sp = getSharedPreferences(Constant.LOGIN_SHARED_DATA_NAME,MODE_PRIVATE);
+        isRegister = false;
 
-        if (sp.getBoolean("user_logged", false)) {
+        if (sp.getBoolean(Constant.USER_LOGGED_BOOLEAN_VALUE_KEY, false)) {
             navigateToHome();
-        } else if (sp.getBoolean("business_logged", false))
+        } else if (sp.getBoolean(Constant.BUSINESS_LOGGED_BOOLEAN_VALUE_KEY, false))
             navigateToBusinessHome();
-
     }
+
 
     @Override
     public void navigateToHome() {
         Intent intent = new Intent(this, UserHomeActivity.class);
         startActivity(intent);
-        finish();
-        try {
+        if (isRegister)
             unregisterReceiver(broadcastReceiver);
-        } catch (Exception ignored) { }
+        finish();
     }
 
 
+    /**
+     * It starts a new BusinessHomeActivity and it shows it to the user. At the end it stops this
+     * activity.
+     */
     private void navigateToBusinessHome() {
         Intent intent = new Intent(this, BusinessHomeActivity.class);
         startActivity(intent);
@@ -68,17 +109,21 @@ public class UserLoginActivity extends LoginActivity {
     @Override
     public void saveUserSession() {
         SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean("user_logged", true);
-        editor.putString("username", username.getText().toString());
+        editor.putBoolean(Constant.USER_LOGGED_BOOLEAN_VALUE_KEY, true);
+        editor.putString(Constant.SD_USERNAME_DATA_KEY, username.getText().toString());
         editor.apply();
     }
 
     @Override
     public void navigateToBusinessLogin() {
         Intent intent = new Intent(this, BusinessLoginActivity.class);
-        registerReceiver(broadcastReceiver, new IntentFilter("finish_activity"));
+        if (!isRegister) {
+            registerReceiver(broadcastReceiver, new IntentFilter(Constant.FINISH_ACTION));
+            isRegister = true;
+        }
         startActivity(intent);
     }
+
 
     @Override
     public void setLoginError() {
@@ -90,14 +135,20 @@ public class UserLoginActivity extends LoginActivity {
         throw new IllegalStateException();
     }
 
+    /**
+     * It handles the user login button click event and it delegate the validation to the
+     * LoginDelegate.
+     */
     @OnClick(R.id.userLoginButton)
     public void onUserLoginButtonClick() {
         mDelegate.userLogin(username.getText().toString(), password.getText().toString());
     }
 
+    /**
+     * It handles the third party login click event.
+     */
     @OnClick(R.id.textViewThirdPartyLogin)
     public void onTextViewThirdPartyLoginClick() {
         mPresenter.businessLogin();
     }
-
 }
--- Implementation/trackMeApplication/app/src/main/java/com/trackme/trackmeapplication/baseUtility/Constant.java
+++ Implementation/trackMeApplication/app/src/main/java/com/trackme/trackmeapplication/baseUtility/Constant.java
@@ -1,8 +1,23 @@
 package com.trackme.trackmeapplication.baseUtility;
 
+/**
+ * Class whit all the constant string value in the project. It allows to a easily refactor of the
+ * messages and a better maintainability of the code.
+ *
+ * @author Mattia Tibaldi
+ *
+ */
 public final class Constant {
 
     public static final String SSN_PATTERN = "^\\d{3}-\\d{2}-\\d{4}$";
     public static final String E_MAIL_PATTERN = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
     public static final String DATE_PATTERN = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
+
+    public static final String FINISH_ACTION = "finish_activity";
+
+    public static final String LOGIN_SHARED_DATA_NAME = "login_data";
+    public static final String SD_USERNAME_DATA_KEY = "username";
+    public static final String SD_EMAIL_DATA_KEY = "email";
+    public static final String USER_LOGGED_BOOLEAN_VALUE_KEY = "user_logged";
+    public static final String BUSINESS_LOGGED_BOOLEAN_VALUE_KEY = "business_logged";
 }
